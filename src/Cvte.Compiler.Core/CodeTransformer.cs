13195ff5ecc1f9a5cb5fba849649f5c6aaf6e42a
walterlv <xblero@gmail.com>
2018-05-24 20:16:06 +0800

    Add document comments.

--- src/Cvte.Compiler.Core/CodeTransformer.cs
+++ src/Cvte.Compiler.Core/CodeTransformer.cs
@@ -10,20 +10,43 @@ using Microsoft.CodeAnalysis.CSharp;
 
 namespace Cvte.Compiler
 {
-    public class CodeTransformer
+    /// <summary>
+    /// 为编译时提供源码转换。
+    /// </summary>
+    internal class CodeTransformer
     {
+        /// <summary>
+        /// 获取转换源码的工作路径。
+        /// </summary>
         private readonly string _workingFolder;
+
+        /// <summary>
+        /// 获取中间文件的生成路径（文件夹，相对路径）。
+        /// </summary>
         private readonly string _intermediateFolder;
+
+        /// <summary>
+        /// 获取所有参与编译的文件。
+        /// </summary>
         private readonly string[] _compilingFiles;
 
-        public CodeTransformer(string workingFolder, string intermediateFolder, IEnumerable<string> compilingFiles)
+        /// <summary>
+        /// 创建用于转换源码的 <see cref="CodeTransformer"/>。
+        /// </summary>
+        /// <param name="workingFolder">转换源码的工作路径。</param>
+        /// <param name="intermediateFolder">中间文件的生成路径（文件夹，相对路径）。</param>
+        /// <param name="compilingFiles">所有参与编译的文件。</param>
+        internal CodeTransformer(string workingFolder, string intermediateFolder, IEnumerable<string> compilingFiles)
         {
             _workingFolder = Path.GetFullPath(workingFolder);
             _intermediateFolder = Path.GetFullPath(Path.Combine(workingFolder, intermediateFolder));
             _compilingFiles = compilingFiles.Select(x => Path.GetFullPath(Path.Combine(workingFolder, x))).ToArray();
         }
 
-        public void Transform()
+        /// <summary>
+        /// 执行代码转换。这将开始从所有的编译文件中搜索 <see cref="CodeTransformAttribute"/>，并执行其转换方法。
+        /// </summary>
+        internal void Transform()
         {
             foreach (var file in _compilingFiles)
             {
@@ -34,29 +57,43 @@ namespace Cvte.Compiler
                 {
                     if (declaration.Attributes.Any(x => x == "CodeTransform"))
                     {
-                        var type = CompileType(declaration.Name, syntaxTree);
-                        var attribute = type.GetCustomAttribute<CodeTransformAttribute>();
-                        var transformer = (IPlainCodeTransformer) Activator.CreateInstance(type);
-
-                        foreach (var sourceFile in attribute.SourceFiles
-                            .Select(x => Path.GetFullPath(Path.Combine(_workingFolder, x))))
-                        {
-                            var fileName = Path.GetFileNameWithoutExtension(sourceFile);
-                            var extension = Path.GetExtension(sourceFile);
-                            foreach (var (i, transformedText) in Enumerable.Range(0, attribute.RepeatCount)
-                                .Select(i => (i, transformer.Transform(File.ReadAllText(sourceFile), new TransformingContext(i)))))
-                            {
-                                var targetFile = Path.Combine(_intermediateFolder, $"{fileName}.g.{i}{extension}");
-                                File.WriteAllText(targetFile, transformedText, Encoding.UTF8);
-                            }
-                        }
-
+                        InvokeCodeTransformer(declaration, syntaxTree);
                     }
                 }
             }
         }
 
+        /// <summary>
+        /// 执行 <see cref="IPlainCodeTransformer"/> 的转换代码的方法。
+        /// </summary>
+        /// <param name="declaration">类声明信息。</param>
+        /// <param name="syntaxTree">整个文件的语法树。</param>
+        private void InvokeCodeTransformer(ClassDeclaration declaration, SyntaxTree syntaxTree)
+        {
+            var type = CompileType(declaration.Name, syntaxTree);
+            var transformer = (IPlainCodeTransformer) Activator.CreateInstance(type);
+            var attribute = type.GetCustomAttribute<CodeTransformAttribute>();
 
+            foreach (var sourceFile in attribute.SourceFiles
+                .Select(x => Path.GetFullPath(Path.Combine(_workingFolder, x))))
+            {
+                var fileName = Path.GetFileNameWithoutExtension(sourceFile);
+                var extension = attribute.TargetType == FileType.Compile ? ".cs" : Path.GetExtension(sourceFile);
+                foreach (var (i, transformedText) in Enumerable.Range(0, attribute.RepeatCount)
+                    .Select(i => (i, transformer.Transform(File.ReadAllText(sourceFile), new TransformingContext(i)))))
+                {
+                    var targetFile = Path.Combine(_intermediateFolder, $"{fileName}.g.{i}{extension}");
+                    File.WriteAllText(targetFile, transformedText, Encoding.UTF8);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 编译指定语法树中的源码，以获取其中定义的类型（类型名称由参数 <paramref name="originalClassName"/> 指定）。
+        /// </summary>
+        /// <param name="originalClassName">通过语法树估算的类型名称。</param>
+        /// <param name="syntaxTree">整个文件的语法树。</param>
+        /// <returns>文件中的转换器的类型。</returns>
         private static Type CompileType(string originalClassName, SyntaxTree syntaxTree)
         {
             var assemblyName = $"{originalClassName}.g";
