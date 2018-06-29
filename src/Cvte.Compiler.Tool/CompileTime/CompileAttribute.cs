1f4babcb8fdae3b8be028fb31572f5e799abb552
walterlv <xblero@gmail.com>
2018-06-29 15:33:52 +0800

    Attribute 增加稍微准确一些的判断

--- src/Cvte.Compiler.Tool/CompileTime/CompileAttribute.cs
+++ src/Cvte.Compiler.Tool/CompileTime/CompileAttribute.cs
@@ -1,4 +1,6 @@
-﻿namespace Cvte.Compiler.CompileTime
+﻿using System;
+
+namespace Cvte.Compiler.CompileTime
 {
     public class CompileAttribute : ICompileAttribute
     {
@@ -8,5 +10,22 @@
         }
 
         public string Name { get; }
+
+        public bool Match<TAttribute>() where TAttribute : Attribute
+        {
+            var attributeName = typeof(TAttribute).Name;
+            if (Name == attributeName)
+            {
+                return true;
+            }
+
+            var index = attributeName.LastIndexOf("Attribute", StringComparison.InvariantCulture);
+            if (index >= 0)
+            {
+                attributeName = attributeName.Substring(0, index);
+            }
+
+            return Name == attributeName;
+        }
     }
 }
--- src/Cvte.Compiler/CompileTime/ICompileAttribute.cs
+++ src/Cvte.Compiler/CompileTime/ICompileAttribute.cs
@@ -1,7 +1,10 @@
-﻿namespace Cvte.Compiler.CompileTime
+﻿using System;
+
+namespace Cvte.Compiler.CompileTime
 {
     public interface ICompileAttribute
     {
         string Name { get; }
+        bool Match<TAttribute>() where TAttribute : Attribute;
     }
 }
