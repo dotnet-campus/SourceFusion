dd1aeb77caf842e3c8ee84b386c30a5d5ce2e901
lindexi <lindexi_gd@163.com>
2018-07-17 20:38:01 +0800

    添加代码

---
+++ src/Cvte.Compiler.Tool/CompileTime/CompileMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cvte.Compiler.CompileTime
+{
+    public class CompileMethod : ICompileMethod
+    {
+        /// <inheritdoc />
+        public CompileMethod(ICompileAttribute[] attributes, string name)
+        {
+            if (ReferenceEquals(attributes, null)) throw new ArgumentNullException(nameof(attributes));
+            if (ReferenceEquals(name, null)) throw new ArgumentNullException(nameof(name));
+            Attributes = attributes;
+            Name = name;
+        }
+
+        /// <inheritdoc />
+        public ICompileAttribute[] Attributes { get; }
+
+        /// <inheritdoc />
+        public string Name { get; }
+    }
+}
---
+++ src/Cvte.Compiler.Tool/CompileTime/CompileProperty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cvte.Compiler.CompileTime
+{
+    internal class CompileProperty: ICompileProperty
+    {
+        /// <inheritdoc />
+        public CompileProperty(string type, ICompileAttribute[] attributes , string name )
+        {
+            if (ReferenceEquals(attributes, null)) throw new ArgumentNullException(nameof(attributes));
+            if (ReferenceEquals(name, null)) throw new ArgumentNullException(nameof(name));
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+            Attributes = attributes;
+            Name = name;
+        }
+
+        public string Type { get;  }
+
+        /// <inheritdoc />
+        public ICompileAttribute[] Attributes { get; }
+
+        public ICompileMethod GetMethod { get; set; }
+
+        public ICompileMethod SetMethod { get; set; }
+
+        /// <inheritdoc />
+        public string Name { get; }
+    }
+}
