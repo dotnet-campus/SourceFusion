380833282a71dd1d688f4e2d240f91f5780dedc5
walterlv <xblero@gmail.com>
2018-12-06 14:57:25 +0800

    Add variable name.

--- sample/dotnetCampus.SourceGenerator/Program.cs
+++ sample/dotnetCampus.SourceGenerator/Program.cs
@@ -12,7 +12,7 @@ namespace dotnetCampus.SourceGenerator
             foreach (var arg in args)
             {
                 var path = Path.GetFullPath(arg);
-                Generate(path, 20000, 100);
+                Generate(path, classCount: 20000, attributedCount: 100);
             }
         }
 
