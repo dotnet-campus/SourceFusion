3e1e2d6ddf3ae5ef444ee882a391e425d33fa9ed
walterlv <xblero@gmail.com>
2018-12-13 11:19:15 +0800

    Use commandlineparser and report more errors.

--- src/SourceFusion.Tool/Cli/Options.cs
+++ src/SourceFusion.Tool/Cli/Options.cs
@@ -7,22 +7,22 @@ namespace dotnetCampus.SourceFusion.Cli
         [Option('p', Required = true, HelpText = "转换源码的工作路径。")]
         public string WorkingFolder { get; set; }
 
-        [Option('i', "IntermediateFolder", HelpText = "SourceFusion 可以使用的临时文件夹路径。")]
+        [Option('i', Required = true, HelpText = "SourceFusion 可以使用的临时文件夹路径。")]
         public string IntermediateFolder { get; set; }
 
-        [Option('g', "GeneratedCodeFolder", HelpText = "SourceFusion 生成的新源码文件所在的文件夹。")]
+        [Option('g', Required = true, HelpText = "SourceFusion 生成的新源码文件所在的文件夹。")]
         public string GeneratedCodeFolder { get; set; }
 
-        [Option('c', "CompilingFiles", HelpText = "所有参与编译的文件，多个文件使用分号（;）分割。")]
+        [Option('c', Required = true, HelpText = "所有参与编译的文件，多个文件使用分号（;）分割。")]
         public string CompilingFiles { get; set; }
 
-        [Option('f', "FilterFiles", HelpText = "只分析指定（正则表达式）名称的文件。")]
+        [Option('f', Required = true, HelpText = "只分析指定（正则表达式）名称的文件。")]
         public string FilterFiles { get; set; }
 
-        [Option('r', "RebuildRequired", HelpText = "如果需要重新生成，则指定为 true。")]
+        [Option('r', Default = true, HelpText = "如果需要重新生成，则指定为 true。")]
         public bool RebuildRequired { get; set; }
 
-        [Option("debug-mode", HelpText = "如果指定，将在启动编译时进入调试模式。")]
+        [Option(Default = false, HelpText = "如果指定，将在启动编译时进入调试模式。")]
         public bool DebugMode { get; set; }
     }
 }
--- src/SourceFusion.Tool/Program.cs
+++ src/SourceFusion.Tool/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.IO;
 using System.Linq;
 using System.Text;
+using CommandLine;
 using dotnetCampus.SourceFusion.Cli;
 using dotnetCampus.SourceFusion.CompileTime;
 using dotnetCampus.SourceFusion.Templates;
@@ -15,92 +17,108 @@ namespace dotnetCampus.SourceFusion
     {
         private static int Main(string[] args)
         {
-            var options = new Options
-            {
-                WorkingFolder = args[1],
-                IntermediateFolder = args[3],
-                GeneratedCodeFolder = args[5],
-                CompilingFiles = args[7],
-                FilterFiles = args[9],
-                RebuildRequired = args[11]?.Equals("true", StringComparison.InvariantCultureIgnoreCase) is true,
-            };
-
-            if (options.DebugMode)
+            if (!Debugger.IsAttached
+                && args.Any(x => x.Equals("--DebugMode", StringComparison.CurrentCultureIgnoreCase)))
             {
                 Debugger.Launch();
             }
 
+            var stopwatch = Stopwatch.StartNew();
+
+            var exitCode = Parser.Default.ParseArguments<Options>(args)
+                .MapResult(Run, HandleError);
+
+            stopwatch.Stop();
+            if (Debugger.IsAttached && !Console.IsInputRedirected)
+            {
+                Console.WriteLine($"预编译耗时：{stopwatch.Elapsed}");
+                Console.WriteLine($"调试模式下已暂停，按任意键结束……");
+                Console.Read();
+            }
+
+            return exitCode;
+        }
+
+        private static int Run(Options options)
+        {
             using (var logger = new Logger())
             {
-                var exitCode = Run(options, logger);
-
-                if (Debugger.IsAttached)
+                try
                 {
-                    Console.WriteLine($"预编译耗时：{logger.Elapsed}");
-                    Console.WriteLine($"调试模式下已暂停，按任意键结束……");
-                    Console.ReadKey();
+                    var exitCode = OnRun(options, logger);
+                    return exitCode;
                 }
-
-                return exitCode;
+                catch (Exception ex)
+                {
+                    logger.Error($"SourceFusion 内部错误：{ex}");
+                    return 0;
+                }
             }
         }
 
-        private static int Run(Options options, ILogger logger)
+        private static int OnRun(Options options, ILogger logger)
         {
-            // Initialize basic command options.
-            //Parser.Default.ParseArguments<Options>(args)
-            //    .WithParsed(options =>
-            //    {
-                    try
+            try
+            {
+                var (workingFolder, intermediateFolder, generatedCodeFolder, compilingFiles) = DeconstructPaths(options);
+                var rebuildRequired = options.RebuildRequired;
+                var cachedExcludesListFile = Path.Combine(intermediateFolder, "Excludes.txt");
+
+                // 如果可以差量编译，那么检测之前已经生成的文件，然后将其直接输出。
+                if (!rebuildRequired && File.Exists(cachedExcludesListFile))
+                {
+                    var cachedExcludeLines = File.ReadAllLines(cachedExcludesListFile, Encoding.UTF8);
+                    foreach (var exclude in cachedExcludeLines)
                     {
-                        var (workingFolder, intermediateFolder, generatedCodeFolder, compilingFiles) = DeconstructPaths(options);
-                        var rebuildRequired = options.RebuildRequired;
-                        var cachedExcludesListFile = Path.Combine(intermediateFolder, "Excludes.txt");
-
-                        // 如果可以差量编译，那么检测之前已经生成的文件，然后将其直接输出。
-                        if (!rebuildRequired && File.Exists(cachedExcludesListFile))
-                        {
-                            var cachedExcludeLines = File.ReadAllLines(cachedExcludesListFile, Encoding.UTF8);
-                            foreach (var exclude in cachedExcludeLines)
-                            {
-                                logger.Message(exclude);
-                            }
-                            return 0;
-                        }
-
-                        var assembly = new CompileAssembly(compilingFiles);
-
-                        // 分析 IPlainCodeTransformer。
-                        var transformer = new CodeTransformer(workingFolder, generatedCodeFolder, assembly);
-                        var excludes = transformer.Transform();
-
-                        // 分析 CompileTimeTemplate。
-                        var templateTransformer = new TemplateTransformer(workingFolder, generatedCodeFolder, assembly);
-                        var templateExcludes = templateTransformer.Transform();
-
-                        var toExcludes = excludes.Union(templateExcludes)
-                            .Select(x => PathEx.MakeRelativePath(workingFolder, x)).ToArray();
-
-                        foreach (var exclude in toExcludes)
-                        {
-                            logger.Message(exclude);
-                        }
-
-                        File.WriteAllLines(cachedExcludesListFile, toExcludes, Encoding.UTF8);
+                        logger.Message(exclude);
                     }
-                    catch (CompilingException ex)
-                    {
-                        foreach (var error in ex.Errors)
-                        {
-                            logger.Error($"error:{error}");
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        logger.Error($"error:{ex}");
-                    }
-                //})
-                //.WithNotParsed(errorList => { });
+                    return 0;
+                }
+
+                var assembly = new CompileAssembly(compilingFiles);
+
+                // 分析 IPlainCodeTransformer。
+                var transformer = new CodeTransformer(workingFolder, generatedCodeFolder, assembly);
+                var excludes = transformer.Transform();
+
+                // 分析 CompileTimeTemplate。
+                var templateTransformer = new TemplateTransformer(workingFolder, generatedCodeFolder, assembly);
+                var templateExcludes = templateTransformer.Transform();
+
+                var toExcludes = excludes.Union(templateExcludes)
+                    .Select(x => PathEx.MakeRelativePath(workingFolder, x)).ToArray();
+
+                foreach (var exclude in toExcludes)
+                {
+                    logger.Message(exclude);
+                }
+
+                File.WriteAllLines(cachedExcludesListFile, toExcludes, Encoding.UTF8);
+            }
+            catch (CompilingException ex)
+            {
+                foreach (var error in ex.Errors)
+                {
+                    logger.Error($"{error}");
+                }
+            }
+
+            return 0;
+        }
+
+        private static int HandleError(IEnumerable<Error> errors)
+        {
+            foreach (var error in errors)
+            {
+                if (error is UnknownOptionError uoe)
+                {
+                    Console.WriteLine($"error: SourceFusion 参数错误：错误的参数名称 {uoe.Token}。");
+                }
+                else
+                {
+                    Console.WriteLine($"error: SourceFusion 参数错误：{error}");
+                }
+            }
 
             return 0;
         }
