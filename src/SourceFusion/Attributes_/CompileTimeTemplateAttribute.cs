af9af43bf1c1e2ecd82a365a26cc66ad2dc2476f
walterlv <xblero@gmail.com>
2019-01-11 09:55:55 +0800

    Allow compiletimetemplate attribute be used multiple times for partial class.

--- src/SourceFusion/Attributes_/CompileTimeTemplateAttribute.cs
+++ src/SourceFusion/Attributes_/CompileTimeTemplateAttribute.cs
@@ -5,7 +5,7 @@ namespace dotnetCampus.SourceFusion
     /// <summary>
     /// 表示一个类是一个类型模板，类中的占位符将在编译后被替换为占位符内实际计算出的代码。
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
     public class CompileTimeTemplateAttribute : Attribute
     {
     }
