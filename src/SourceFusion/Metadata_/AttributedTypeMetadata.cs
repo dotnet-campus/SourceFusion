8f745eb3cadf856a84169bed2966511639b453bd
walterlv <xblero@gmail.com>
2019-01-14 21:29:31 +0800

    Move type.

--- src/SourceFusion/Metadata_/AttributedTypeMetadata.cs
+++ src/SourceFusion/Metadata_/AttributedTypeMetadata.cs
@@ -1,20 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace dotnetCampus.SourceFusion
 {
-    public class AttributedTypeMetadataCollection<T, TAttribute> : Collection<AttributedTypeMetadata<T, TAttribute>>
-        where TAttribute : Attribute
-    {
-        public static implicit operator List<(Type, TAttribute)>(
-            AttributedTypeMetadataCollection<T, TAttribute> collection)
-        {
-            return collection.Select(x => (x.Type, x.Attribute)).ToList();
-        }
-    }
-
     public class AttributedTypeMetadata<T, TAttribute> where TAttribute : Attribute
     {
         internal AttributedTypeMetadata(Type type, Func<T> creator, TAttribute attribute)
