a41ca85c38404728429fd867a907b6c5c6a5141f
walterlv <xblero@gmail.com>
2019-01-21 11:32:28 +0800

    CompilingException 参数判空

--- src/SourceFusion.Tool/Core/CompilingException.cs
+++ src/SourceFusion.Tool/Core/CompilingException.cs
@@ -12,7 +12,7 @@ namespace dotnetCampus.SourceFusion.Core
         /// <param name="errors"></param>
         public CompilingException(IEnumerable<string> errors) : base("编译过程中出现了异常。")
         {
-            Errors = errors.ToList();
+            Errors = errors?.ToList() ?? throw new ArgumentNullException(nameof(errors));
         }
 
         /// <summary>
@@ -21,7 +21,7 @@ namespace dotnetCampus.SourceFusion.Core
         /// <param name="errors"></param>
         public CompilingException(params string[] errors)
         {
-            Errors = errors;
+            Errors = errors ?? throw new ArgumentNullException(nameof(errors));
         }
 
         public IReadOnlyList<string> Errors { get; }
