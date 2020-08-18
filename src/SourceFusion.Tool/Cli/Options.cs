a3fb03796db7cad37ea9a96fbd2134e5dd4ab809
lindexi <lindexi_gd@163.com>
2020-08-18 14:27:58 +0800

    更改职业学院的命令行库

--- src/SourceFusion.Tool/Cli/Options.cs
+++ src/SourceFusion.Tool/Cli/Options.cs
@@ -1,31 +1,29 @@
 ﻿using System;
-using CommandLine;
+using dotnetCampus.Cli;
 
 namespace dotnetCampus.SourceFusion.Cli
 {
     internal class Options
     {
-        [Value(0, HelpText = "转换源码的工作路径。")]
+        [Value(0, Description = "转换源码的工作路径。")]
         public string WorkingFolder { get; set; }
 
-        [Option('t', "tool-folder", HelpText = "SourceFusion 可以使用的临时文件夹路径。")]
+        [Option('t', "tool-folder", Description = "SourceFusion 可以使用的临时文件夹路径。")]
         public string ToolFolder { get; set; }
 
-        [Option('c', "generated-code-folder", HelpText = "SourceFusion 生成的新源码文件所在的文件夹。")]
+        [Option('c', "generated-code-folder", Description = "SourceFusion 生成的新源码文件所在的文件夹。")]
         public string GeneratedCodeFolder { get; set; }
 
-        [Option('p', "project-property-file", HelpText = "一个文件，包含项目的各种所需属性和集合。")]
+        [Option('p', "project-property-file", Description = "一个文件，包含项目的各种所需属性和集合。")]
         public string ProjectPropertyFile { get; set; }
         
-        [Option('s', "preprocessor-symbols", HelpText = "预处理符号，也就是条件编译符。")]
+        [Option('s', "preprocessor-symbols", Description = "预处理符号，也就是条件编译符。")]
         public string PreprocessorSymbols { get; set; }
 
-        [Option('r', "rebuild", HelpText = "如果需要重新生成，则指定为 true。")]
-        public string _rebuildRequired { get; set; }
+        [Option('r', "rebuild", Description = "如果需要重新生成，则指定为 true。")]
+        public bool RebuildRequired { get; set; }
 
-        public bool RebuildRequired => _rebuildRequired?.Equals("true", StringComparison.OrdinalIgnoreCase) == true;
-
-        [Option("debug-mode", HelpText = "如果指定，将在启动编译时进入调试模式。")]
+        [Option("debug-mode", Description = "如果指定，将在启动编译时进入调试模式。")]
         public bool DebugMode { get; set; }
     }
 }
