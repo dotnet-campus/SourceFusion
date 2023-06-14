b44b283955840b1c498dade10e03aab25d73eb33
lindexi <lindexi_gd@163.com>
2023-06-14 14:28:04 +0800

    加上注释

--- src/TelescopeSourceGenerator/Analyzers/Context_/AssemblyCandidateClassParseResult.cs
+++ src/TelescopeSourceGenerator/Analyzers/Context_/AssemblyCandidateClassParseResult.cs
@@ -5,6 +5,9 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace dotnetCampus.Telescope.SourceGeneratorAnalyzers;
 
+/// <summary>
+/// 程序集里可能被标记导出的类型
+/// </summary>
 readonly struct AssemblyCandidateClassParseResult
 {
     public AssemblyCandidateClassParseResult(INamedTypeSymbol exportedTypeSymbol, ImmutableArray<AttributeData> attributes, GeneratorSyntaxContext generatorSyntaxContext)
@@ -18,7 +21,7 @@ readonly struct AssemblyCandidateClassParseResult
     public AssemblyCandidateClassParseResult()
     {
         Success = false;
-        ExportedTypeSymbol = default;
+        ExportedTypeSymbol = default!; // 这个瞬间就被过滤掉了，先不考虑可空的复杂写法了
         Attributes = default;
         GeneratorSyntaxContext = default;
     }
@@ -33,9 +36,15 @@ readonly struct AssemblyCandidateClassParseResult
     /// </summary>
     public ClassDeclarationSyntax ExportedTypeClassDeclarationSyntax => (ClassDeclarationSyntax) GeneratorSyntaxContext.Node;
 
+    /// <summary>
+    /// 类型标记的特性
+    /// </summary>
     public ImmutableArray<AttributeData> Attributes { get; }
 
     public GeneratorSyntaxContext GeneratorSyntaxContext { get; }
 
+    /// <summary>
+    /// 是否成功，用于过滤掉不满足条件的对象
+    /// </summary>
     public bool Success { get; }
 }
--- src/TelescopeSourceGenerator/Analyzers/Context_/MarkClassParseResult.cs
+++ src/TelescopeSourceGenerator/Analyzers/Context_/MarkClassParseResult.cs
@@ -1,5 +1,8 @@
 ﻿namespace dotnetCampus.Telescope.SourceGeneratorAnalyzers;
 
+/// <summary>
+/// 被标记的类型的转换结果，约等于生成代码前的最终结果
+/// </summary>
 readonly struct MarkClassParseResult
 {
     public MarkClassParseResult(AssemblyCandidateClassParseResult classParseResult,
@@ -9,7 +12,13 @@ readonly struct MarkClassParseResult
         MarkExportAttributeParseResult = markExportAttributeParseResult;
     }
 
+    /// <summary>
+    /// 定义在程序集里面的类型
+    /// </summary>
     public AssemblyCandidateClassParseResult ClassParseResult { get; }
 
+    /// <summary>
+    /// 程序集特性里面的定义结果
+    /// </summary>
     public MarkExportAttributeParseResult MarkExportAttributeParseResult { get; }
 }
