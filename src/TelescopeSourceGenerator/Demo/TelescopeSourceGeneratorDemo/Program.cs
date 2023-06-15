726131a158bea1d6929d555550bd41f762707b76
lindexi <lindexi_gd@163.com>
2023-06-15 11:47:06 +0800

    加上更复杂的测试

--- src/TelescopeSourceGenerator/Demo/TelescopeSourceGeneratorDemo/Program.cs
+++ src/TelescopeSourceGenerator/Demo/TelescopeSourceGeneratorDemo/Program.cs
@@ -14,7 +14,7 @@ internal class Program
     }
 }
 
-[Foo(1ul, FooEnum.N2, Number2 = 2L, Type = typeof(Foo))]
+[Foo(1ul, FooEnum.N2, typeof(Base), null, Number2 = 2L, Type2 = typeof(Foo), FooEnum2 = FooEnum.N1, Type3 = null)]
 class Foo : Base
 {
 }
@@ -25,18 +25,23 @@ class Base
 
 class FooAttribute : Attribute
 {
-    public FooAttribute(ulong number1, FooEnum fooEnum)
+    public FooAttribute(ulong number1, FooEnum fooEnum, Type? type1,Type? type3)
     {
         Number1 = number1;
-        FooEnum = fooEnum;
+        FooEnum1 = fooEnum;
+        Type1 = type1;
     }
 
     public ulong Number1 { get; set; }
     public long Number2 { get; set; }
 
-    public FooEnum FooEnum { get; set; }
+    public FooEnum FooEnum1 { get; set; }
+    public FooEnum FooEnum2 { get; set; }
+    public FooEnum FooEnum3 { get; set; }
 
-    public Type? Type { get; set; }
+    public Type? Type1 { get; set; }
+    public Type? Type2 { get; set; }
+    public Type? Type3 { get; set; }
 }
 
 public enum FooEnum
